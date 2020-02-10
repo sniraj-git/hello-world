46fc2d173910e0ac852e32a39b53fec8f1b3c53b
Pratik Mohanty <pmohanty@marvell.com>
2020-02-10 14:53:54 +0000

    CQ : Short Summary Description (one line)
    ******** For Release Notes **********
    
    Not Required.
    * [CQXXXX]  : Short summary.
    
      Resolution : Solution description.
      Scope      : 45000/41000 Series Adapters
    
    ****** For Internal Purposes ********
    
    PROBLEM DESCRIPTION:
    
    *************************************
     Panic Backtrace if any
    *************************************
    
    SOLUTION:
    
    Signed-off-by: Shreyas Deodhar <sdeodhar@marvell.com>
    Reviewed-by: Santosh Vernekar <svernekar@marvell.com>
    
    git commit -a

---
+++ cloneing.py
@@ -0,0 +1,9 @@
+import os
+os.system("git status")
+os.system("ls")
+os.system("touch new1.py")
+os.system("git add new1.py")
+os.system("git commit -a")
+os.system("git push origin master")
+os.system("git status")
+
